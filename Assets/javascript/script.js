0c2138b52b878e9e07be8a542fdf66a7b87b087b
Lili Cohen <lili.cohen.tv@gmail.com>
2020-10-22 02:00:48 -0700

    adding prompts for the screen

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -1,14 +1,31 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+
+  var userLenChars = prompt("How many password characters between 8 to 128 do you want?");
+  var userLowerChars= confirm("Would you like to include lowercase characters?");
+  var userUpperChars= confirm("Would you like to include uppercase characters?");
+  var userNumericChars= confirm("Would you like to include numeric characters?");
+  var userSpecialChars= confirm("Would you like to include special characters?");
+
+  var computerLenChars = 1;
+  var computerLowerChars = "abcdefghijklmnopqrstuvwxyz";
+  var computerUpperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var computerNumericChars = "123456789";
+  var computerSpecialChars = "!@#$%^&*()_+";
+
+
+  
+  var password = generatePassword(
+    
+  );
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
